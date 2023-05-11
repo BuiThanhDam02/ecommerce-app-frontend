3f577efc25085ff6caaaebe176d057786bd102b0
letrongtinh27 <20130440@st.hcmuaf.edu.vn>
2023-05-11 19:49:45 +0700

    Logout
    Đăng xuất, account menu

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,19 +1,21 @@
-const AuthReducer = (state = {AuthData: null,loading: false , error : false},action) =>{
-    switch (action.type) {
-        case "LOGIN_START":
-            return {...state, loading : true,error:false}
-            
-        case "LOGIN_SUCCESS":
-            
-            return {...state,AuthData : action.data, loading: false, error : false}
-            
-        case "LOGIN_FAIL":
-            return {...state, loading: false, error : true}
-           
-       
-        default:
-            return state
-            
-    }
+const AuthReducer = (
+  state = { AuthData: null, loading: false, error: false },
+  action
+) => {
+  switch (action.type) {
+    case "LOGIN_START":
+      return { ...state, loading: true, error: false };
+
+    case "LOGIN_SUCCESS":
+      return { ...state, AuthData: action.data, loading: false, error: false };
+
+    case "LOGIN_FAIL":
+      return { ...state, loading: false, error: true };
+    case "LOGOUT":
+      return { ...state, AuthData: action.data, loading: false, error: false };
+
+    default:
+      return state;
+  }
 };
 export default AuthReducer;
