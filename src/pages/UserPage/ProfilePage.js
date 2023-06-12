1bcc05bb787d7837489a1ac70db8ab18db09c147
Bunthanhdam <115606214+BuiThanhDam02@users.noreply.github.com>
2023-06-12 16:56:16 +0700

    checkout action

---
+++ src/pages/UserPage/ProfilePage.js
@@ -0,0 +1,11 @@
+import Profile from "../../components/User/Profile";
+
+const profilePage = () => {
+  return (
+    <div className="login-container">
+      <Profile />
+    </div>
+  );
+};
+
+export default profilePage;
